cf69bbce8cba5a54c0d027b6636bd52556468bac
Facundo Caamaño <facundoadriancaamano@outlook.com>
2023-01-30 20:30:51 -0300

    segunda etrega final

--- src/app.js
+++ src/app.js
@@ -1,4 +1,10 @@
 import express from 'express';
+import handlebars from 'express-handlebars';
+import __dirname from './utils.js';
+import { Server } from 'socket.io';
+import mongoose from 'mongoose'
+import chatRouter from './routes/chat.router.js';
+import messagesModel from './dao/models/messages.model.js';
 import cartsRouter from './routes/carts.router.js';
 import productsRouter from './routes/products.router.js';
 
@@ -6,7 +12,55 @@ const app = express();
 
 app.use(express.json())
 
-app.use('/api/carts', cartsRouter)
-app.use('/api/products', productsRouter)
+app.use(express.urlencoded({ extended: true }))
 
-app.listen(8080, ()=>{console.log("Servidor corriendo..");});
+app.use(express.static(__dirname + '/public'))
+
+app.engine('handlebars' , handlebars.engine())
+
+app.set('views', __dirname + '/views')
+app.set('view engine', 'handlebars')
+
+const uri = 'mongodb+srv://FacundoCaamano:LyMXNeB3ETCiOiyu@cluster0.iwosz6p.mongodb.net/?retryWrites=true&w=majority'
+
+mongoose.set({strictQuery: true})
+mongoose.connect(uri,{dbName: 'ecommerce'}, async (error)=>{
+    if (!error){
+        console.log('Conectado a la base de datos');
+        const httpServer = app.listen(8080, ()=>{
+            console.log("Servidor corriendo...");
+        });
+
+        const socketServer = new Server(httpServer)
+
+        let messages = []
+
+        socketServer.on('connection', socket =>{
+            socket.on('msg_front', data => console.log(data));
+            socket.emit('msg_back',"Conectado al servicio")
+            
+
+            socket.on('session', async data =>{
+                messages = await messagesModel.find().lean().exec();
+                socketServer.emit('first',messages)
+            })
+
+
+            socket.on('message', async data=>{
+                await messagesModel.create(data)
+                messages = await messagesModel.find().lean().exec();
+                socketServer.emit('logs',messages)
+                })
+        })
+
+        app.use((req,res,next)=>{
+            req.io = socketServer
+            next()
+        })
+        app.use('/api/chat', chatRouter)
+        app.use('/api/products', productsRouter)
+        app.use('/api/carts', cartsRouter)
+
+    } else {    console.log("ups!! no se pudo conectar a la base de datos");
+            }
+} )
--- src/routes/products.router.js
+++ src/routes/products.router.js
@@ -3,47 +3,72 @@ import {Router} from 'express'
 const router = Router()
 
 
-import ProductManager from '../productManager.js';
+import ProductManager from '../dao/manager/db/productManager.js';
 const manager = new ProductManager('./productos.json');
 
 
 router.get('/', async (req, res) => {
-    const products = await manager.get()
+    
     let limit = req.query.limit
-    if (!limit) res.send({products})
-    else {
-        const prodLimit = [];
-        if (limit > products.length) limit = products.length;
-        for (let index = 0; index < limit; index++) {
-            prodLimit.push(products[index]);
-        }
-        res.send({prodLimit})
-    }
+    let page = req.query.page
+    let query = req.query.query
+    let sort = req.query.sort
+
+    const products = await manager.get(limit, page, sort, query)
+    res.render('product-pages',products)
+    
+        req.io.emit('update', products) 
+
 })
+
+
 router.post('/', async (req, res) => {
     const {title, description, price, thumbnails, code, stock, category, status} = req.body
     const addProduct = await manager.add(title, description, price, code, stock, category, status, thumbnails)
+    req.io.emit('update',await manager.get())
     res.send(addProduct)
 })
 
 router.get('/:pid', async (req, res) => {
     const id = req.params.pid
     const product = await manager.getById(id)
-    res.send({product})
+    res.render('product-detail',{ product})
+    
 })
 
 
 router.put('/:pid', async (req, res) => {
-    const id = parseInt(req.params.pid)
+    const id = req.params.pid
     const {title, description, price, thumbnails, code, stock, category, status} = req.body
     const updateProduct = await manager.updateById(id, title, description, price, code, stock, category, status, thumbnails)
+    req.io.emit('update', await manager.get())
     res.send(updateProduct)
 })
 
 router.delete('/:pid', async (req, res) => {
-    const id = parseInt(req.params.pid)
+    const id = req.params.pid
     const deleteProduct =  await manager.deleteById(id)
+    req.io.emit('update', await manager.get())
     res.send(deleteProduct)
 })
+router.get('/productosTR', async (req, res) =>{
+    const products = await manager.get()
+    res.render('productosTR',
+    {
+        title: "lista",
+        products: products.payload
+    })
+
+})
+
+router.get('/home', async (req, res) =>{
+    const products = await manager.get()
+    res.render('home',
+    {
+        title: "lista",
+        products: products.payload
+    })
+})
+
 
 export default router;
